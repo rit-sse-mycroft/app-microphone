e6d26397a9273629788ded926741fe6e885d4a8d
Jesse Jurman <j.r.jurman@gmail.com>
2014-01-17 16:32:32 -0500

    herp derp code

--- microphone.rb
+++ microphone.rb
@@ -16,7 +16,21 @@ class Microphone < Mycroft::Client
   end
 
   def on_data(data)
-    # Your code here
+    puts data
+    parsed = parse_message(data)
+    if parsed[:type] == 'APP_MANIFEST_OK' || parsed[:type] == 'APP_MANIFEST_FAIL'
+      check_manifest(parsed)
+      @verified = true
+      # we should send our grammer here
+      
+    elsif parsed[:type] == 'MSG_QUERY' and parsed[:action] == "stream request"
+      clientIp = parsed[:data]["ip"]
+      clientPort = parsed[:data]["port"]
+
+      # run vlc UDP using the client IP and Port
+      `vlc dshow:// --sout=#transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=44100}:udp{dst=#{clientIp}:#{clientPort}}`
+    end
+
   end
 
   def on_end
