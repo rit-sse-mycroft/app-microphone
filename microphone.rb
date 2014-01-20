1449bbb0e2c2d2c04d4d061ffd8de1fbb9d7d106
kristenmills <kristen@kristen-mills.com>
2014-01-20 15:04:41 -0500

    removed check manifest

--- microphone.rb
+++ microphone.rb
@@ -15,12 +15,8 @@ class Microphone < Mycroft::Client
     # Your code here
   end
 
-  def on_data(data)
-    puts data
-    parsed = parse_message(data)
+  def on_data(parsed)
     if parsed[:type] == 'APP_MANIFEST_OK' || parsed[:type] == 'APP_MANIFEST_FAIL'
-      check_manifest(parsed)
-      @verified = true
       query('stt', 'request_stt', {})
       # we should send our grammer here
       
