02c88b14acd613c9be911499d48bda58037afa87
kristenmills <kristen@kristen-mills.com>
2014-02-01 22:30:28 -0500

    Updated to use new mycroft gem

--- microphone.rb
+++ microphone.rb
@@ -13,25 +13,18 @@ class Microphone < Mycroft::Client
     super
   end
 
-  def connect
-    # Your code here
+  on 'MSG_QUERY' do |data|
+    client_ip = data['data']["ip"]
+    client_port = data['data']["port"]
+
+    # run vlc UDP using the client IP and Port
+    `ffmpeg -ac 1 -f dshow -i audio="YOUR MICROPHONE" -ar 16000 -acodec pcm_s16le -f rtp rtp://#{client_ip}:#{client_port}`
   end
 
-  def on_data(parsed)
-    if parsed[:type] == 'MSG_QUERY'
-      client_ip = parsed[:data]['data']["ip"]
-      client_port = parsed[:data]['data']["port"]
-
-      # run vlc UDP using the client IP and Port
-      `ffmpeg -ac 1 -f dshow -i audio="YOUR MICROPHONE" -ar 16000 -acodec pcm_s16le -f rtp rtp://#{client_ip}:#{client_port}`
-    elsif parsed[:type] == 'APP_DEPENDENCY'
-      up
-    end
+  on 'APP_DEPENDENCY' do |data|
+    up
   end
 
-  def on_end
-    # Your code here
-  end
 end
 
 Mycroft.start(Microphone)
